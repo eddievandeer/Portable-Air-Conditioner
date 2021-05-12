13cdab31898f54b8cf825bd6b9c86d1dda6b1b66
eddievandeer <1255173159@qq.com>
2021-05-12 20:32:30 +0800

    feat(footer): add project address

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,11 @@
     <div class="w-full h-auto mt-8 text-sm flex flex-col justify-center items-center">
         <p class="text-gray-500">
             ©
-            <a class="hover:underline" href="">Portable Air Conditioner</a>
+            <a class="hover:underline" href="https://github.com/eddievandeer/Portable-Air-Conditioner" target="__blank">
+                Portable Air Conditioner
+            </a>
             -
-            <a class="hover:underline" href="https://vivekblog.vercel.app/">Vivek</a>
+            <a class="hover:underline" href="https://vivekblog.vercel.app/" target="__blank">Vivek</a>
         </p>
         <p class="text-gray-500">2021 - present</p>
     </div>
