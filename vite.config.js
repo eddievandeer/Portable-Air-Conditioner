0f28b1c6f89ab60c44d9efd4e7e396f9683968a3
eddievandeer <1255173159@qq.com>
2021-05-14 09:30:16 +0800

    build: add aliases to utils and store

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  resolve: {
+    alias: {
+      '@utils': path.join(__dirname, './src/utils'),
+      '@store': path.join(__dirname, './src/store')
+    }
+  },
   plugins: [vue()]
 })
